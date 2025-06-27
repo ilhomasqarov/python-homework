4039418e76545b9d12830d14ee78a10e6e858060
ilhomasqarov <ilkhomas2000@gmail.com>
2025-06-27 03:00:57 -0700

    Update lesson1.py

--- lesson-1/homework/lesson1.py
+++ lesson-1/homework/lesson1.py
@@ -1,1 +1,30 @@
+length = float(input('To‘rtburchakning bo‘yi: '))
+width = float(input('To‘rtburchakning eni: '))
+perimeter = 2 * (length + width)
+print('To‘rtburchakning perimetri:', perimeter)
 
+
+boyi = str(input('tortburchakning boyi: '))
+eni = str(input('tortburchakning eni: '))
+p = 2 * (boyi + eni)
+s = boyi * eni
+print('tortburchakning perimetri:', p)
+print('tortburchakning yuzi :', s)
+
+diametr = (input('yumaloqning diametri: '))
+f = (f'math.pi' * diametr)
+print ('diameter':, f)
+
+a = 3
+b = 6
+mean = (a+b)/2
+print (mean)
+
+a = 3
+b = 6
+sum = a + b
+square1 = a * a 
+square2 = b * b
+print (sum)
+print (square1)
+print (square2)
